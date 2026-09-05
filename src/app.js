4eb5f28798d07d06ed3308bb50f0673908c6543c
profchauchuty <cesar.chauchuty@escola.pr.gov.br>
2026-09-05 13:48:43 -0300

    test render

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Success!'})
+})
+
+app.listen(80)
