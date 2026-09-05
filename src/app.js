fa2f8219a28fd30be84d353182410054f567964d
profchauchuty <cesar.chauchuty@escola.pr.gov.br>
2026-09-05 14:37:54 -0300

    update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import express from 'express'
 import cors from 'cors'
+import sql from './database/conn.js'
 
 const app = express()
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.json({ hello: 'World!'})
+app.get('/:username/:password', async (req, res) => {
+    const { username, password } = req.params
+    const [newUser] = await sql`INSERT INTO "USER" VALUES (DEFAULT, ${username}, ${password}) RETURNING ID, USERNAME`
+    res.json({
+        username,
+        password,
+        newUser
+    })
 })
 
 
---
+++ src/database/conn.js
@@ -0,0 +1,9 @@
+import postgres from 'postgres'
+
+const urlConnection = process.env.DATABASE_URL
+console.log(urlConnection)
+const sql = postgres(urlConnection, {
+    ssl: true
+})
+
+export default sql
